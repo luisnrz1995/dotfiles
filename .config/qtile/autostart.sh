f106a5b43a5680ace16d3cfef1d68198a54de2a4
Luis Narváez <luisnarvaezmadera@gmail.com>
2023-05-21 21:55:10 -0500

    removing unnecessary programs from autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+xset r rate 300 50 &
 nitrogen --restore & # set wallpaper.
 xset r rate 300 50 & # set keyboard repeat delay and rate.
-picom &
-pgrep -x panel > /dev/null &
+touchpad_config -e 11 &
+# picom &
+redshift LAT:LON &
+mate-polkit &
+keepassxc-autostart &
+pkill keepassxc-autos &
