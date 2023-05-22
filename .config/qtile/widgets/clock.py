95a4d21ba30e8ab08ee0e7a4ca741adbc143c18a
Luis Narváez <luisnarvaezmadera@gmail.com>
2023-05-21 21:51:26 -0500

    remove unnecessary script

--- .config/qtile/widgets/clock.py
+++
@@ -1,9 +0,0 @@
-# Import the 'time' module
-import time
-
-# Define a function 'clock' that returns the current time in a specific format
-def clock():
-  # Using 'strftime' method of 'time' module to format the time
-  current_time = time.strftime("%H:%M")
-  # Returning the formatted time with an additional emoji
-  return " " + current_time
